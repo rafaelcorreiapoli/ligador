cbf4cdf05dae0096e3c416bc19787799c760f110
rafaelcorreiapoli <rafael.correia.poli@gmail.com>
2016-03-14 15:48:47 -0300

    first commit

---
+++ mlr-alunos/src/linker/Pass1.java
@@ -0,0 +1,96 @@
+package linker;
+
+import java.io.IOException;
+
+import mvn.util.LinkerSymbolTable;
+
+/**
+ * Passo 1 do ligador.<br>
+ * Nesse passo é criada a tabela de símbolos do linker, passando por todos os
+ * arquivos.<br>
+ * Por questões de simplicidade, existem algumas simplificações no linker:
+ * - cada arquivo apresenta uma e apenas uma origem relocável;
+ * - os conflitos de código absoluto não são verificados;
+ * @author FLevy
+ * @version 23.10.2006 
+ * Preparação do arquivo para alunos - PSMuniz 1.11.2006
+ * @version 01.01.2010 : atualização da classe de acordo com a definição dos slides (Tiago)
+ */
+public class Pass1 extends Pass {
+
+    /**A tabela de símbolos para o linker*/
+    private LinkerSymbolTable symbolTable;
+    /**A posição relocável anterior*/
+    private int relativeLocationCouter;
+    /**A base de relocação a ser considerada no código*/
+    private int base;
+    /**Numera as diferentes variáveis externas existentes num arquivo*/
+    private int externalCounter = 0;
+
+    public Pass1() {
+        symbolTable = new LinkerSymbolTable();
+        relativeLocationCouter = 0;
+        base = 0;
+    }
+
+    /**
+     * Processa uma linha de código.
+     *
+     * @param nibble O nibble com as informações do endereço da linha.
+     * @param address O endereço da linha (sem o nibble).
+     * @param code O código da linha.
+     * @param currentFile O arquivo atual que está sendo processado.
+     * @return Verdadeiro caso a análise teve sucesso, falso caso contrário.
+     * @exception Caso tenha ocorrido algum problema de IO.
+     */
+    protected boolean processCode(int nibble, String address, String code, String currentFile){
+        /**
+         * TODO: processCode
+         *
+         * Analisar o nibble e incrementar o endereçamento relativo (relativeLocationCounter) de acordo.
+         *
+         */
+        return false;
+    }//
+
+    /**
+     * Processa uma linha de endereço simbólico.
+     *
+     * @param nibble O nibble com as informações do endereço da linha.
+     * @param address O endereço referente ao símbolo (sem nibble).
+     * @param symbol O símbolo em si.
+     * @param currentFile O arquivo atual que esta sendo processado.
+     * @return Verdadeiro caso a análise teve sucesso, falso caso contrário.
+     * @throws IOException
+     *             Caso tenha ocorrido algum problema de IO.
+     */
+    protected boolean processSymbolicalAddress(int nibble, String address, String symbol, String currentFile, String originalLine)
+            throws IOException {
+        /*
+         * TODO: processSymbolicalAddress
+         * Situações típicas: entry point relocável, entry point absoluto e external.
+         * O objetivo é resolver os entry points e externals relativos.
+         * Devem ser avaliadas as combinações apropriadas do nibble.
+         * Deve-se calcular o endereço do codigo e atualizar apropriadamente a tabela de simbolos.
+         */
+
+        return false;
+    }//
+
+    /**
+     * Finaliza o arquivo lido (pode haver um próximo arquivo).
+     */
+    protected void fileEnd() {
+
+        /*
+         * TODO: fileEnd()
+         * Quando há mudança de arquivo, deve-se mudar a base!
+         * Atualizar as variáveis existentes de acordo.
+         *
+         * */
+    }//
+
+    public LinkerSymbolTable getSymbolTable() {
+        return symbolTable;
+    }
+}
---
+++ mlr-alunos/src/linker/Pass2.java
@@ -0,0 +1,97 @@
+package linker;
+
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+import mvn.util.LinkerSymbolTable;
+
+/**
+ * Passo 2 do ligador.<br>
+ * Nesse passo é gerado o código objeto a partir da tabela
+ * de símbolos obtida.
+ * @author FLevy
+ * @version 23.10.2006
+ * Preparação do arquivo para alunos - PSMuniz 1.11.2006
+ * @version 01.01.2010 : atualização da classe de acordo com a definição dos slides. (Tiago)
+ */
+public class Pass2 extends Pass {
+
+    /**Gerencia o arquivo de saída*/
+    private Output out;
+    /**Tabela de símbolos utilizada pelo Linker*/
+    private LinkerSymbolTable symbolTable;
+    /**Indica o endereçamento corrente da parte relocável do código. */
+    private int relativeLocationCouter;
+    /**A base de relocação a ser considerada no código. */
+    private int base;
+
+    /**Contador de variáveis externas que não foram resolvidas*/
+    private int externalCounter = 0;
+
+
+    public Pass2(LinkerSymbolTable symbolTable, String objFile) throws IOException {
+        out = new Output(objFile);
+        this.symbolTable = symbolTable;
+        relativeLocationCouter = 0;
+        base = 0;
+    }
+
+    /**
+     * Processa uma linha de código.
+     *
+     * @param nibble O nibble do endereço da linha.
+     * @param address O endereço da linha (sem o nibble).
+     * @param code O código da linha.
+     * @param currentFile O arquivo atual que está sendo processado.
+     * @return Verdadeiro caso a análise teve sucesso, falso caso contrario.
+     * @exception Caso tenha ocorrido algum problema de IO.
+     */
+    protected boolean processCode(int nibble, String address, String code, String currentFile)
+            throws IOException {
+        /*
+         * TODO: processCode
+         *
+         * Aqui, deve-se gerar o código objeto a partir da tabela de simbolos (ST).
+         * Deve-se avaliar as combinações apropriadas do nibble. Se houver pendência (relocável
+         * ou absoluta), ela deve ter ser resolvida e seu valor inserido na ST.
+         * O código resolvido deve ser enviado para a saída.
+         *
+         */
+
+        return false;
+
+    }//method
+
+    protected boolean processSymbolicalAddress(int nibble, String address, String symbol, String currentFile, String originalLine)
+            throws IOException {
+        
+        /**
+         * TODO: processSymbolicalAddress
+         * Tratamento do Endereçamento simbólico. 
+         * Caso EntryPoint: escreve no arquivo de saída
+         * Caso External: se resolvido ignora, caso contrário insere na tabela o arquivo com um novo endereçamento "virtual"
+         *                  e escreve o external no arquivo de saída.
+         * 
+         *          
+         */
+
+        //Se for símbolo exportável: gero no arquivo de saída as informações a respeito dele
+        //...
+        //...
+        return false;
+    }
+
+    /**
+     * Finaliza o arquivo lido (pode haver um próximo arquivo).
+     */
+    protected void fileEnd() {
+        /*
+         * TODO: fileEnd()
+         * Quando há mudança de arquivo, deve-se atualizar a base e o relativeLocationCounter!
+         * */
+    }
+
+    public void closeOutput() throws IOException {
+        out.close();
+    }
+}
